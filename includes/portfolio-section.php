ec6e3cb0c3db3cd91d32f79dcbd271e778efe56e
Michael Hernando <mhernando334@gmail.com>
2023-12-10 11:43:31 -0500

    Image size fixes

--- includes/portfolio-section.php
+++ includes/portfolio-section.php
@@ -12,27 +12,27 @@ if( $featured_portfolio_items ): ?>
         $project_title = get_field( 'project_title', $portfolio_items->ID ); 
         $description = get_field( 'description', $portfolio_items->ID ); 
         ?>
-    <div class="card m-2" style="width: 18rem;">
-        <img class="card-img-top" src="<?php echo $thumbnail; ?>" alt="Card image cap">
-        <div class="card-body">
-            <h5 class="card-title"><?php echo $project_title; ?></h5>
-            <p class="card-text"><?php echo $description; ?></p>
-            <div class="row">
-                <div class="col-6 text-start">
-                    <?php if ($github = get_field( 'github', $portfolio_items->ID )) : ?>
-                        <a href="<?php echo $github; ?>" class="btn btn-link">
-                        <i class="bi bi-github" style="font-size: 20px; color: #5A5A5A;"></i>
-                        </a>
-                    <?php endif; ?>
-                </div>
-                <div class="col-6 ">
-                    <?php if ($link = get_field( 'link', $portfolio_items->ID )) : ?>
-                        <a href="<?php echo $link; ?>" class="btn btn-primary">View More</a>
-                    <?php endif; ?>
+        <div class="card m-2" style="width: 18rem;">
+            <img class="card-img-top mt-2" src="<?php echo $thumbnail; ?>" alt="Card image cap" style="height: 200px; width: 100%; object-fit: cover;">
+            <div class="card-body">
+                <h5 class="card-title"><?php echo $project_title; ?></h5>
+                <p class="card-text"><?php echo $description; ?></p>
+                <div class="row">
+                    <div class="col-6 text-start">
+                        <?php if ($github = get_field( 'github', $portfolio_items->ID )) : ?>
+                            <a href="<?php echo $github; ?>" class="btn btn-link">
+                                <i class="bi bi-github" style="font-size: 20px; color: #5A5A5A;"></i>
+                            </a>
+                        <?php endif; ?>
+                    </div>
+                    <div class="col-6 ">
+                        <?php if ($link = get_field( 'link', $portfolio_items->ID )) : ?>
+                            <a href="<?php echo $link; ?>" class="btn btn-primary">View More</a>
+                        <?php endif; ?>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
     <?php endforeach; ?>
 </div>
 <?php endif; ?>
